fea5172ba655ce1b930c9200041205982e766af7
mannanswe <147591955+mannanswe@users.noreply.github.com>
2024-07-31 10:53:10 +0600

    layout design

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
-import { getIronSession } from 'iron-session';
-import { sessionOptions } from '@/lib/session';
+import { NextResponse } from "next/server";
+import { getIronSession } from "iron-session";
+import { sessionOptions } from "@/lib/session";
 
 // Auth Middleware: Protect certain routes
 export async function middleware(req) {
@@ -11,18 +11,18 @@ export async function middleware(req) {
   const isLoggedIn = session.isLoggedIn;
 
   // Guest Middleware: Redirect authenticated users from login and register pages
-  if ((url.pathname === '/login' || url.pathname === '/register') && isLoggedIn) {
-    return NextResponse.redirect(new URL('/dashboard', req.url));
-  }
+  // if ((url.pathname === '/login' || url.pathname === '/register') && isLoggedIn) {
+  //   return NextResponse.redirect(new URL('/dashboard', req.url));
+  // }
 
   // Auth Middleware: Redirect unauthenticated users from protected routes
-  if ((url.pathname.startsWith('/protected') || url.pathname === '/dashboard') && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+  // if ((url.pathname.startsWith('/protected') || url.pathname === '/dashboard') && !isLoggedIn) {
+  //   return NextResponse.redirect(new URL('/login', req.url));
+  // }
 
   return res;
 }
 
-export const config = {
-  matcher: ['/protected/:path*', '/dashboard', '/login', '/register'],
-};
+// export const config = {
+//   matcher: ['/protected/:path*', '/dashboard', '/login', '/register'],
+// };
